6b11a71d301295484c2e24bbc0f320929cc137b1
Aljomaa <sybhunter21@gmail.com>
2025-06-30 16:29:15 -0700

    Delete utils directory

--- utils/db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from config import MONGO_URI
-
-client = MongoClient(MONGO_URI)
-db = client["islamic_bot"]
-
-# جداول/Collections
-user_col = db["users"]        # المستخدمين والمفضلات
-comp_col = db["complaints"]   # الشكاوى والاقتراحات
